5048f1b37ed22d692f0b0c751344c75ff03e7249
alexiscasmi <acastillomilian7@gmail.com>
2025-11-28 18:41:10 -0500

    Add files via upload

---
+++ styles.py
@@ -0,0 +1,14 @@
+INPUT_STYLE_COMPACT = {
+    "width": "100%",
+    "padding": "6px 10px",
+    "fontSize": "1rem",
+    "borderRadius": "8px",
+    "border": "1px solid #ccc",
+}
+
+INFO_CARD_STYLE = {
+    "padding": "20px",
+    "backgroundColor": "white",
+    "borderRadius": "16px",
+    "boxShadow": "4px 4px 12px rgba(0,0,0,0.15)",
+}
